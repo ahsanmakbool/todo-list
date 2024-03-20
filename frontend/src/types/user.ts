85097ba254b998eb16f552967507c13a63433419
ahsan <ahsanmaqbool9188@gmail.com>
2024-03-20 18:58:13 +0500

    complete the frontend and the backend

---
+++ frontend/src/types/user.ts
@@ -0,0 +1,22 @@
+interface User {
+  _id?: string;
+  email: string;
+  username?: string;
+  password: string;
+  confirmPassword?: string;
+}
+
+interface UserState {
+  loading?: boolean;
+  error?: string;
+  user?: User | null;
+  isAuthenticated?: boolean;
+  isRegistered?: boolean;
+  clearError: () => void;
+  signup: (formData: User) => void;
+  signin: (formData: User) => void;
+  logout: () => void;
+  loadUser: () => void;
+}
+
+export type { User, UserState };
